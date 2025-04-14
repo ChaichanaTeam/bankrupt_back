e7e967c6b8772c4f27baf9e7b86a40b049329597
os3k1mtr1l <rud.vlad2015@hotmail.com>
2025-04-15 01:04:18 +0200

    kk3

--- src/api/utils/mail.py
+++ src/api/utils/mail.py
@@ -2,9 +2,20 @@ import smtplib
 from email.message import EmailMessage
 from src.core.config import settings
 
-smpt_server = smtplib.SMTP("smtp.gmail.com", 587)
-smpt_server.starttls()
-smpt_server.login(settings.EMAIL, settings.EMAIL_PASSWORD)
+class EmailServer:
+    def __init__(self):
+        self.smtp: smtplib.SMTP = smtplib.SMTP("smtp.gmail.com", 587)
+        self.smtp.starttls()
+        self.smtp.login(settings.EMAIL, settings.EMAIL_PASSWORD)
+    
+    def send_message(self, msg: EmailMessage) -> None:
+        self.smtp.send_message(msg)
+
+    def __del__(self):
+        self.smtp.quit()
+    
+
+email_service: EmailServer = EmailServer()
 
 def send_verification_email(email: str, token: str):
     msg = EmailMessage()
@@ -18,6 +29,6 @@ def send_verification_email(email: str, token: str):
     )
 
     try:
-            smpt_server.send_message(msg)
+        email_service.send_message(msg)
     except Exception as e:
         print(f"Mail has not been sended: {e}")
