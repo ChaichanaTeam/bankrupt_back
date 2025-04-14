9b9d7c0388e12e1f79269df1e0a6aaa364844b02
os3k1mtr1l <rud.vlad2015@hotmail.com>
2025-04-15 00:45:49 +0200

    Refactored for public use

--- src/api/utils/mail.py
+++ src/api/utils/mail.py
@@ -1,22 +1,23 @@
-##
 import smtplib
 from email.message import EmailMessage
+from src.core.config import settings
+
+smpt_server = smtplib.SMTP("smtp.gmail.com", 587)
+smpt_server.starttls()
+smpt_server.login(settings.EMAIL, settings.EMAIL_PASSWORD)
 
 def send_verification_email(email: str, token: str):
     msg = EmailMessage()
     msg["Subject"] = "Email verification"
-    msg["From"] = "#"
+    msg["From"] = settings.EMAIL
     msg["To"] = email
+
     msg.set_content(
         f"Hi!\n\nPlease, verify your email on this link:\n"
         f"http://localhost:8000/verify-email?token={token}"
     )
 
     try:
-        with smtplib.SMTP("smtp.gmail.com", 587) as server:
-            server.starttls()
-            server.login("#", "#")
-            server.send_message(msg)
+            smpt_server.send_message(msg)
     except Exception as e:
         print(f"Mail has not been sended: {e}")
-##
--- src/core/config.py
+++ src/core/config.py
@@ -4,5 +4,7 @@ class Settings:
     SECRET_KEY: str = os.getenv("SECRET_KEY", "fallback-key")
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    EMAIL: str = os.getenv("EMAIL")
+    EMAIL_PASSWORD: str = os.getenv("EMAIL_PASSWORD")
 
 settings = Settings()
