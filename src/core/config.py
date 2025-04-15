580ea501ff5cc1930a377a750b722b02a8b0efd8
os3k1mtr1l <rud.vlad2015@hotmail.com>
2025-04-15 17:58:55 +0200

    Config updated

--- src/core/config.py
+++ src/core/config.py
@@ -1,9 +1,17 @@
 import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+env_path: Path = Path(os.path.dirname(__file__)) / ".env" / "var.env"
+
+if env_path.exists():
+    load_dotenv(dotenv_path=env_path)
 
 class Settings:
+    DATABASE_URL: str = "sqlite:///./bank.db"
     SECRET_KEY: str = os.getenv("SECRET_KEY", "fallback-key")
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     EMAIL: str = os.getenv("EMAIL")
     EMAIL_PASSWORD: str = os.getenv("EMAIL_PASSWORD")
     IS_DEPLOYED = os.getenv("DEPLOY") is not None
