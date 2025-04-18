031dd822a1453407f3b84617e64e3a561d81f741
os3k1mtr1l <rud.vlad2015@hotmail.com>
2025-04-18 22:01:31 +0200

    Initial commit

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ app: FastAPI = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"], # Debug purpose, will change - settings.ORIGINS
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
