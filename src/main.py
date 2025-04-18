71cc1150c8c7e61f5da4ccdec4137233199ef636
os3k1mtr1l <rud.vlad2015@hotmail.com>
2025-04-18 18:32:06 +0200

    Test

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ app: FastAPI = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
