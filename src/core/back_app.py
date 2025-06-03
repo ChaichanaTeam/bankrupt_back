6d00425d8c76f0c9764bfa7091e3115bb3be8e21
os3k1mtr1l <rud.vlad2015@hotmail.com>
2025-06-03 23:15:55 +0200

    COMMUNIST STAR WAS A THREAT

--- src/core/back_app.py
+++ src/core/back_app.py
@@ -4,24 +4,21 @@ from fastapi.middleware.cors import CORSMiddleware
 from src.db.session import engine
 from src.db.base import Base
 from src.api.routes import user as user_routes
-# from src.api.routes import wallet as wallet_routes
 from src.api.routes import cleanup as jobs_routes
 from src.api.routes import cards as card_routes
 from src.api.routes import admin as admin_routes
 from src.api.routes import savings as savings_routes
 from src.api.routes import bills as bills_routes
 from typing import AsyncGenerator, Any
+from src.core.config import settings
 
-# async def lifespan(app: FastAPI) -> AsyncGenerator[None, Any, None]:
-    
-#     yield
 
 class BackendApp(FastAPI):
     def __init__(self):
         super().__init__()
         super().add_middleware(
             CORSMiddleware,
-            allow_origins=["*"], #settings.ORIGINS
+            allow_origins=settings.ORIGINS,
             allow_credentials=True,
             allow_methods=["*"],
             allow_headers=["*"],
@@ -33,7 +30,6 @@ class BackendApp(FastAPI):
 
     def __initializeRoutes(self, app: FastAPI) -> None:
         app.include_router(user_routes.router, prefix="/auth", tags=["auth"])
-        # app.include_router(wallet_routes.router, prefix="/account", tags=["account"])
         app.include_router(jobs_routes.router, tags=["miscallenious"])
         app.include_router(card_routes.router, prefix="/card", tags=["card"])
         app.include_router(admin_routes.router, prefix="/admin", tags=["admin"])
